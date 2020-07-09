997e00a60e3520850b695b114a89380b09143c75
Blanca Nallely Macias Vazquez <naychida@gmail.com>
2020-07-09 10:22:12 -0500

    Programa 2 Finalizado

--- prog2.py
+++ prog2.py
@@ -2,6 +2,6 @@ num1 = int(input("Dame un numero: "))
 num2 = int(input("Dame otro numero: "))
 
 resul = 0
-for num in range(num1,num2):
+for num in range(num1,num2+1):
     resul += num
 print(f'La suma es {resul}')
